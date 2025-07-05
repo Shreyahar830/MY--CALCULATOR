5fe40f087fb11e1a6726ce40665f842b0ecc4b01
shreya kapoor <shreyakoushal777@gmail.com>
2025-07-05 15:05:00 +0530

    Add files via upload
    This is my  First Project ....

---
+++ index.js
@@ -0,0 +1,47 @@
+ let buttons = document.querySelectorAll("button");
+ let sc = document.querySelector("#sc")
+
+ buttons.forEach(function(btn){
+
+ btn.addEventListener("click" , function(event){
+
+    if ( event.target.innerText === "=" ){
+         try{
+                 let ans = eval(sc.value);
+                 sc.value = ans;
+
+         }catch (erroe){
+            alter("Wrong expression enter")
+            sc.value = "";
+         }
+        
+    } else if (event.target.innerText === "DEL"){
+         sc.value = sc.value.slice(0, -1);
+
+         
+    } else if (event.target.innerText === "AC"){
+      sc.value = "";
+    }
+    else{
+
+       sc.value += event.target.innerText;  
+    }
+     
+
+    }) ;   
+
+        
+    });
+
+    
+
+
+
+
+
+
+
+
+
+ 
+
